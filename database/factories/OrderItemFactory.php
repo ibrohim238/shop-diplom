f5e472306c1fbb5b4279b01706b828475ec7e4dd
ibroh <ialeroev238@mail.ru>
2025-05-06 06:10:06 +0300

    Refactor OrderItemFactory to improve data generation logic
    Replaced hardcoded `order_id` with a factory relationship to `Order` for better test data consistency. Adjusted quantity and total_amount generators to use appropriate ranges, improving realism of generated data. Removed unnecessary imports and timestamps for cleaner code.

--- database/factories/OrderItemFactory.php
+++ database/factories/OrderItemFactory.php
@@ -2,10 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Carbon;
 
 /* @mixin OrderItem */
 class OrderItemFactory extends Factory
@@ -15,12 +15,10 @@ class OrderItemFactory extends Factory
     public function definition(): array
     {
         return [
-            'order_id' => $this->faker->randomNumber(),
-            'quantity' => $this->faker->randomNumber(),
-            'total_amount' => $this->faker->randomFloat(),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
+            'quantity' => $this->faker->numberBetween(0,100),
+            'total_amount' => $this->faker->numberBetween(0,10000),
 
+            'order_id' => Order::factory(),
             'product_id' => Product::factory(),
         ];
     }
